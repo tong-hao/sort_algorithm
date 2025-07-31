ab52e17191071087d7177482b54c96c7679bdb7e
haotong <tong.hao@hotmail.com>
2025-07-31 15:02:17 +0800

    fix:heap sort

--- src/HeapSort.h
+++ src/HeapSort.h
@@ -4,59 +4,42 @@
 
 #include "utils/StringUtil.h"
 
+#include <vector>
+#include <iostream>
+
 // 堆化函数
-// 将双亲调整为最大元素
-template <typename T> void heapify(std::vector<T> &data, int begin, int end) {
-  if (begin >= end) {
-    return;
-  }
+template <typename T>
+void heapify(std::vector<T>& vct, int r, int n){
+    int root = r;
+    int left = 2*r + 1;
+    int right = 2*r + 2;
 
-  int root = begin;
-  int left = 2 * begin + 1;
-  int right = left + 1;
+    int max = root;
+    if(left < n && vct[left] > vct[max]) {
+        max = left;
+    }
+    if(right < n && vct[right] > vct[max]) {
+        max = right;
+    }
 
-  // 从双亲、左孩子、右孩子中找最大的
-  int max = root;
-  if (left <= end && data[left] > data[max]) {
-    max = left;
-  }
-  if (right <= end && data[right] > data[max]) {
-    max = right;
-  }
-  if (max == root) {
-    return;
-  }
-
-  // 将双亲调整为最大的
-  std::swap(data[root], data[max]);
-
-  // 继续调整max位置的结点
-  heapify(data, max, end);
+    if(max != root) {
+        std::swap(vct[root], vct[max]);
+        h(vct, max, n);
+    }
 }
 
-template <typename T> void sort(std::vector<T> &data) {
-  if (data.size() <= 1)
-    return;
+template <typename T>
+void sort(std::vector<T>& vct) {
+    // 1.construct heap
+    int n = vct.size();
+    for(int i = vct.size()/2-1; i >=0; i--) {
+        h(vct, i, n);
+    }
 
-  // 1. Construct heap
-  // root结点是最大的
-  // 注意：root >= 0
-  // 从最后一个叶子结点开始
-  for (int root = data.size() / 2 - 1; root >= 0; root--) {
-    heapify(data, root, data.size() - 1);
-    print<T>(data);
-  }
-
-  // 2. pop and heapify loop
-  // 主要：end > 0，因为最后一个元素不需要调整
-  for (int end = data.size() - 1; end > 0; end--) {
-    std::cout << "->" << end << std::endl;
-    // 将root结点调整到end位置
-    std::swap(data[0], data[end]);
-    print<T>(data);
-
-    // 再次执行堆化函数
-    heapify(data, 0, end - 1);
-    print<T>(data);
-  }
+    // 2. pop and heapify loop
+    for(int i = n -1; i >=0; i--){
+        std::swap(vct[i], vct[0]);
+        h(vct, 0, i);
+    }
 }
+
